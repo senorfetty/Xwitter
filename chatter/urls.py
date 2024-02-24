038c9da176c057907c6825e936dd5800d96f1013
senorfetty <billykimbett@gmail.com>
2024-02-24 14:07:40 +0300

    authentication

--- chatter/urls.py
+++ chatter/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.welcome, name='welcome'),
     path('signup', views.signup, name= 'signup'),
     path('login', views.log, name= 'login'),
+    path('home', views.home, name= 'home'),
 ]
--- chatter/views.py
+++ chatter/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from .forms import RegForm
-
-# Create your views here
+from django.contrib.auth import authenticate,login
+from django.contrib import messages
 
 def welcome(request):
     return render(request, 'welcome.html')
@@ -12,11 +12,30 @@ def signup(request):
         
         if form.is_valid():
             form.save()
-            return redirect ('login')
+            return redirect('login')
         
     else:
         form = RegForm()
+        
     return render(request, 'signup.html', {'form': form})
 
 def log(request):
-    return render(request, 'login.html')
+    if request.method == 'POST':
+        username= request.POST.get('username')
+        password= request.POST.get('password')
+        
+        user= authenticate(username=username, password=password)
+        
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        
+        else:
+            messages.error(request,'Username or Password is incorrect')
+            return redirect('login')
+    
+    else:
+        return render(request, 'login.html')
+
+def home(request):
+    return render(request, 'home.html')
